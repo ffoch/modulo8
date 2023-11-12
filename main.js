ec261457f766065922866b6723c0b18083ebc2fe
Felipe Foch <felipe.foch@hotmail.com>
2023-11-12 19:43:01 -0300

    Modificacao no html

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 const form = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./images/aprovado.png" atl="Emoji celebrando" />';
+const imgReprovado = '<img src="./images/reprovado.png" atl="Emoji decepcionado" />';
+
+let linhas = '';
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
-
+   
     let linha = '<tr>';
     linha += `<td>${inputNomeAtividade.value}</td>`;
     linha += `<td>${inputNotaAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value >= 7 ? 'Aprovado' : 'Reprovado'}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado :  imgReprovado}</td>`;
     linha += '</tr>';
 
+    linhas += linha;
+
     const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linha;
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
 });
